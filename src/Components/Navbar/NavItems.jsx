2941493407cf92c081f84ea2d329fd2549991f8c
Irfan Azhar <muhammad.irfan.azhar@mail.ugm.ac.id>
2021-07-10 17:24:29 +0700

    edit competition page and replace how to register pages with booklet

--- src/Components/Navbar/NavItems.jsx
+++ src/Components/Navbar/NavItems.jsx
@@ -24,7 +24,7 @@ function NavItems(props){
             <NavItem onChange={handleClick} url="/" screen= {props.screen} name="Home" />
             <NavItem onChange={handleClick} url="/about" screen= {props.screen} name="About" />
             <DropDown onChange={handleClick} name="Events" items={dropDownEvents}/>
-            <NavItem onChange={handleClick} url="/how-to-register" screen= {props.screen} name="How to register" />
+            <NavItem onChange={handleClick} url="/competition-booklet" screen= {props.screen} name="Booklet" />
             <DropDown  onChange={handleClick} name="Registrations" items={dropDownRegistrations}/>
         </div>
 
--- src/Pages/Competition.jsx
+++ src/Pages/Competition.jsx
@@ -1,50 +1,79 @@
-import {Object2, Object1, Group2} from "../Ornaments/Ornaments"
+import {Object1, Group2} from "../Ornaments/Ornaments"
 import Timeline from '../Components/Homepage/Timeline/TimelineCompetition'
 import {LightBulbIcon, AcademicCap, BookOpen } from '../Components/Homepage/icons'
-import {Link} from 'react-router-dom'
-import {rules, registGuidline, writingGuidline} from '../Data/Competition'
+import { Link } from 'react-router-dom'
 
 export default function Competition () {
     return(
         <div className="overflow-hidden">
-            <div className="h-screen bg-putih bg-opacity-10 bg-cover" style={{ backgroundImage: `url("https://images.unsplash.com/23/black-white-city.JPG?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=667&q=80")` }}>
-                    <div className="h-screen relative z-0 md:px-32 pt-10 flex flex-col md:flex-row items-center">
-                        <img className="z-20 h-48 md:h-60 m-5 border-r-4 border-b-4 border-ungu-300 rounded-2xl shadow-lg" src="https://cdn.pixabay.com/photo/2015/07/17/22/43/student-849822_960_720.jpg" alt="" />
-                        <div className="z-20 m-5 text-ungu-300">
-                                <p className="text-4xl font-serif font-bold">ASEAN Innovative Policy Competition (AIPC)</p>
-                                <div className="flex">
-                                    <Link to="/registration/competition">
-                                        <div className="px-2 py-1 m-3 w-24 text-center rounded shaddow bg-ungu-200 hover:bg-ungu-400 text-lg text-putih hover:text-white inline-block transition duration-500 ease-in-out transform hover:scale-125">
-                                            <p>Register</p>
-                                        </div>
-
-                                    </Link>
-
-                                    <Link to="/competition-booklet">
-                                        <div className="px-2 py-1 m-3 w-24 text-center rounded shaddow bg-ungu-200 hover:bg-ungu-400 text-lg text-putih hover:text-white inline-block transition duration-500 ease-in-out transform hover:scale-125">
-                                            <p>Booklet</p>
-                                        </div>
-                                    </Link>
-
+            <div className="bg-putih overflow-hidden" >
+                    <div className="relative md:px-28 px-5 pt-5">
+                        <div className="z-10 text-ungu-300">
+                            <h1 className="md:text-5xl text-4xl font-bold font-serif text-center my-5">ASEAN Innovative Policy Competition (AIPC)</h1>
+                            <p className="text-justify text-lg font-serif">
+                                ASEAN Innovative Policy Competition (AIPC) is a competition for undergraduate students across ASEAN to challenge their critical thinking skills regarding several social issues. AIPC also provides exclusive conference and mentorship for the participants. This year, AIPC focuses on the three pillars of ASEAN: Political-Security Community (APC), Economic Community (AEC), and Socio-Cultural Community (ASC) with the topics as follows:
+                            </p>
+                            <div className="md:flex md:justify-evenly">
+                                <div 
+                                    className="relative bg-cover mx-auto w-80 h-44 my-5 m-5 border-r-4 border-b-4 border-ungu-300 rounded-2xl shadow-lg mb-12" 
+                                    style={{ backgroundImage: `url("https://imgdb.in/iMCx.jpg")` }}>
+                                    <p className="absolute text-lg font-semibold md:inset-y-1/3 inset-y-1/2 inset-x-1/4 md:text-lg text-white font-serif font-semibold text-center z-10 mx-2">Fash Fashion</p>
+                                    <div
+                                        className="bg-ungu-300 absolute top-0 left-0 w-full h-full opacity-30 rounded-lg"
+                                    />
                                 </div>
-                                <p className="font-semibold">The series of Competition Public Action 2021 events consists of two main events, namely ASEAN Innovative Policy Competition, International Public Action Conference, and the side event namely Public Festival. For the ASEAN Innovative Policy Competition, it consists of three main issues: Sustainable Fashion; Indigenous Peoples Empowerment; and Freedom of Speech and Democracy in the Digital Era.</p>
-                        </div>
-                        <div className="absolute hidden md:block -top-32 -left-72 z-0">
-                            <Object2 />
-                        </div>
-                        <div className="z-0 hidden md:block absolute -top-12 -right-20">
-                            <Object1 />
-                        </div>
-                        <div className="absolute -right-32 -bottom-16 transform -rotate-12 z-10">
-                            <Object1 />
-                        </div>
-                        <div className="absolute bg-ungu-200 w-screen h-12 left-0 bottom-0"/>
-                        <div className="absolute bg-putih bg-opacity-50 top-0 left-0 w-full h-full -z-20"></div>
+
+                                <div 
+                                    className="relative bg-cover mx-auto w-80 h-44 my-5 m-5 border-r-4 border-b-4 border-ungu-300 rounded-2xl shadow-lg" 
+                                    style={{ backgroundImage: `url("https://imgdb.in/iMCx.jpg")` }}>
+                                    <p className="absolute text-lg font-semibold md:inset-y-1/3 inset-y-1/3 inset-x-1/4 md:text-lg text-white font-serif font-semibold text-center z-10 mx-2">Indigenous People</p>
+                                    <div
+                                        className="bg-ungu-300 absolute top-0 left-0 w-full h-full opacity-30 rounded-lg"
+                                    />
+                                </div>
+                                <div 
+                                    className="relative bg-cover mx-auto w-80 h-44 my-5 m-5 border-r-4 border-b-4 border-ungu-300 rounded-2xl shadow-lg" 
+                                    style={{ backgroundImage: `url("https://imgdb.in/iMCx.jpg")` }}>
+                                    <p className="absolute md:inset-y-1/3 inset-y-1/3 inset-x-1/5 md:text-lg text-white font-serif font-semibold text-center z-10 mx-2">Freedom of speech and democracy in the digital era</p>
+                                    <div
+                                        className="bg-ungu-300 absolute top-0 left-0 w-full h-full opacity-30 rounded-lg"
+                                    />
+                                </div>
+                            </div>
+                            <p className="text-justify font-serif text-lg">
+                            by involving youths and experts from different countrie, we hope to broaden the perspectives regarding the aforementioned issues
+                            </p>
+
+                            <div className="flex justify-center mb-12">
+                                <Link to="/registration/competition">
+                                    <div className="px-2 py-1 m-3 w-24 text-center rounded shaddow bg-ungu-200 hover:bg-ungu-400 text-lg text-putih hover:text-white inline-block transition duration-500 ease-in-out transform hover:scale-125">
+                                        <p>Register</p>
+                                    </div>
+
+                                </Link>
+
+                                <Link to="/competition-booklet">
+                                    <div className="px-2 py-1 m-3 w-24 text-center rounded shaddow bg-ungu-200 hover:bg-ungu-400 text-lg text-putih hover:text-white inline-block transition duration-500 ease-in-out transform hover:scale-125">
+                                        <p>Booklet</p>
+                                    </div>
+                                </Link>
+
+                            </div>
+                                <div className="hidden md:block absolute -top-12 -left-44 transform -rotate-90">
+                                        <Object1 />
+                                </div>
+                                <div className="absolute -right-32 -bottom-16 transform -rotate-12 hidden md:block">
+                                        <Object1 />
+                                </div>
+                            </div>
+
+
                     </div>
                     
             </div>
 
             <div className="md:h-screen">
+                <div className="w-screen h-6 bg-ungu-400"/>
 				<div className="md:h-1/2 h-96 flex flex-col bg-putih text-center justify-evenly items-center relative">	
 					<p className="text-3xl text-ungu-400 font-bold pb-12"> About Competition </p>
 					<p className="text-lg text-ungu-300">ASEAN Innovative Policy Competition (AIPC)</p>
@@ -59,59 +88,21 @@ export default function Competition () {
 						<div className="flex flex-col items-center bg-putih shadow max-w-max rounded-lg text-center p-4 m-4">
 							<LightBulbIcon />
 							<p className="text-ungu-200 max-w-xs"> Giving opportunity for undergraduate students across ASEAN to demonstrate their critical thinking skills </p>
-							</div>
+						</div>
 						<div className="flex flex-col items-center bg-putih shadow max-w-max rounded-lg text-center p-4 m-4">
 							<AcademicCap />
 							<p className="text-ungu-200 max-w-xs"> Building the engagement and solidarity of ASEAN communities towards perpetual social issues </p>
-							</div>
+						</div>
 						<div className="flex flex-col items-center bg-putih shadow max-w-max rounded-lg text-center p-4 m-4">
 							<BookOpen />
 							<p className="text-ungu-200 max-w-xs"> Supporting the new generation of policy makers through mentorship </p>
-							</div>
+						</div>
 					</div>
 				</div>	
 			</div>
 			<div className=" bg-putih">
 				<Timeline />
 			</div>
-            <div className="bg-ungu-400 text-white py-10 text-justify">
-                <h2 className="text-3xl font-serif font-bold text-center my-5">Rules</h2>
-                <ul className="px-5 md:px-20">
-                    {
-                        rules.map((rule) =>
-                        <li className="my-3 text-lg">‣ {rule}</li>
-                                            
-                    )}
-
-                </ul>
-
-            </div>
-
-            <div className="bg-oren text-white py-10 text-justify">
-                <h2 className="text-3xl font-serif font-bold text-center my-5">Registration Guidelines</h2>
-                <ol className="px-5 md:px-20">
-                    {
-                        registGuidline.map((registGuidline, index) =>
-                        <li className="my-3 text-lg" key={index}> {index + 1}. {registGuidline}</li>
-                                            
-                    )}
-
-                </ol>
-
-            </div>
-            
-            <div className="bg-putih text-ungu-40 py-10 text-justify">
-                <h2 className="text-3xl font-serif font-bold text-center my-5">Writing Guidelines</h2>
-                <ol className="px-5 md:px-20">
-                    {
-                        writingGuidline.map((writingGuidline, index) =>
-                        <li className="my-3 text-lg" key={index}> {index + 1}. {writingGuidline}</li>
-                                            
-                    )}
-
-                </ol>
-
-            </div>
         </div>
     )
 }
