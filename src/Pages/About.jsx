0b9bbf2d3a3e1816802cb773610a220657613089
Irfan Azhar <muhammad.irfan.azhar@mail.ugm.ac.id>
2021-07-11 16:15:05 +0700

    edit about page desc

--- src/Pages/About.jsx
+++ src/Pages/About.jsx
@@ -1,4 +1,4 @@
-import {Object1, Group2, Object2} from "../Ornaments/Ornaments"
+import {Object1, Group2} from "../Ornaments/Ornaments"
 
 export default function About () {
     return(
@@ -10,14 +10,12 @@ export default function About () {
                         <h1 className="text-left text-5xl font-bold font-serif mx-4">About</h1>
                         <Group2 />
                     </div>
-                    <p className="my-5 z-20"> Public Action is an annual event held by the students of the Department of Public Policy and Management, Faculty of Social and Political Sciences, Universitas Gadjah Mada. The theme for the Public Action 2021 event is “Innovative Youths in Action: Emphasizing Public Value in Addressing Perpetual Social Issues”. Through this theme, we intend to gather youth across ASEAN with diverse cultural backgrounds who are determined to solve a myriad of contemporary issues.</p>
-                    <p className="my-5 z-20">The series of Competition Public Action 2021 events consists of two main events, such as ASEAN Innovative Policy Competition, International Public Action Conference, and the side event namely Public Festival. For the ASEAN Innovative Policy Competition, it consists of three main issues: Sustainable Fashion; Indigenous Peoples Empowerment; and Freedom of Speech and Democracy in the Digital Era.</p>
+                    <p className="my-5 z-20">Public Action is an annual event organized by students of the Department of Public Policy and Management in the Faculty of Social and Political Sciences, Universitas Gadjah Mada. This event is aimed to build awareness toward public issues in engaging ways.</p>
+                    <p className="my-5 z-20">The grand theme of Public Action 2021 is “Innovative Youths in Action: Emphasizing Public Value in Addressing Perpetual Social Issues”. The ASEAN communities have been struggling with a myriad of perpetual social issues. The ASEAN Pillars are also derived from the actual problems that are faced by the society and become concerns among ASEAN member states. Hence, policies and projects intended to solve the concerns of these pillars must include public values—philosophy that puts emphasis on the public interest—as the main foundation. Public Action intends to make impacts to society by emphasizing on the public value in every event conducted. </p>
+                    <p className="my-5 z-20">ASEAN Innovative Policy Competition (AIPC) is focused on the three pillars of ASEAN: Political-Security Community (APC), Economic Community (AEC), and Socio-Cultural Community (ASC). By involving youths and experts from different countries, we hope to broaden the perspectives in solving the aforementioned issues. Thus, we also intend to involve local communities in Public Action’s side events, such as micro, small and medium enterprises (MSMEs), street performers, and underprivileged children. Public Action is expected to be enjoyed by various groups of people, from the locals to ASEAN communities.</p>
                 </div>
                 <div className="absolute -right-32 -bottom-28 transform z-0 transform rotate-90">
                     <Object1 />
-                </div>
-                <div className="absolute hidden md:block -bottom-3/4 -left-60">
-                    <Object2 />
                 </div>
 
 
